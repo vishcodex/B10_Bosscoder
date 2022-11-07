b8a2e943f748631cecbf070d1d2d809332d1d9ff
Putta, Vishnu <vishnu.putta@hotmail.com>
2022-11-06 23:26:32 -0600

    Brute force approach

--- middle-of-the-linked-list.py
+++ middle-of-the-linked-list.py
@@ -3,6 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Brute force approach
+def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    count = 0
+    tmp = head
+    while tmp:
+        count+=1
+        tmp = tmp.next
+        
+    middle = count//2
+    l = 0
+    while l < middle:
+        head = head.next
+        l+=1
+    return head
+
+
+# Two pointer solution
 class Solution:
     # def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
