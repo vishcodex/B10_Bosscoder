12fbcd5ed549c73872aedde836192ac5aebf8993
Putta, Vishnu <vishnu.putta@hotmail.com>
2022-11-11 07:35:32 -0600

    Solution

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[left] = nums[i]
+                left += 1
+        return left
