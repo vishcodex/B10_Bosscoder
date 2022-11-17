3e53041346532e562f3b34ad7f2ec29a5df55e1c
Putta, Vishnu <vishnu.putta@hotmail.com>
2022-11-16 22:56:17 -0600

    Solution

---
+++ sqrtx.py
@@ -0,0 +1,8 @@
+def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        for i in range(1,x+1):
+            if i * i == x:
+                return i
+            elif i * i > x:
+                return (i-1)
