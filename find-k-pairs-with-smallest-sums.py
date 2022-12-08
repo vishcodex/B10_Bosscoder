4060dd6ecc0ce0bcfa6f066dbc577e0bb14bac37
Putta, Vishnu <vishnu.putta@hotmail.com>
2022-12-07 21:47:09 -0600

    Updated Solution

--- find-k-pairs-with-smallest-sums.py
+++ find-k-pairs-with-smallest-sums.py
@@ -2,6 +2,9 @@ from typing import List
 import heapq
 
 class Solution:
+
+    # Solution 1 : timeout exception
+
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
         if not nums2 or not nums1: return []
         heap = []
@@ -13,6 +16,13 @@ class Solution:
                 print("num1+num2, num1, num2 :",  i, num1+num2, num1, num2)
                 heapq.heappush(heap, [num1+num2, num1, num2])
         return [x[1:] for x in heapq.nsmallest(k, heap)]
+
+    # Solution 2
+
+    def kSmallestPairs(self, nums1, nums2, k):
+        streams = map(lambda u: ([u+v, u, v] for v in nums2), nums1)
+        stream = heapq.merge(*streams)
+        return [suv[1:] for suv in itertools.islice(stream, k)]
 
 
 
